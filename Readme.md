831ac8c61d31d31057f27fefd3b326642734aed3
lorenypsum <loren.ypsum@gmail.com>
2026-07-14 05:25:17 -0300

    front-end

--- Readme.md
+++
@@ -1,8 +0,0 @@
-# Ai Wallet
-
-## About Database
--- cargo sqlx migrate create assets
--- cargo sqlx migrate info
--- cargo sqlx migrate run
--- cargo sqlx migrate revert
--- cargo sqlx migrate --help
--- src/routes/front_end.rs
+++ src/routes/front_end.rs
@@ -8,8 +8,8 @@ use crate::{
 use askama::Template;
 use axum::{
     Form, Router,
-    response::{Html, IntoResponse, Redirect, Response},
-    routing::{get, post},
+    response::{Html, IntoResponse, Redirect},
+    routing::get,
 };
 use axum_extra::extract::{CookieJar, cookie::Cookie};
 use serde::Deserialize;
@@ -17,11 +17,19 @@ use tokio::try_join;
 
 pub fn router() -> Router<AppState> {
     Router::new()
-        .route("/", get(index))
+        .route("/", get(home))
         .route("/assets", get(assets).post(purchase_asset))
         .route("/login", get(login_page).post(login))
         .route("/logout", get(logout))
         .route("/register", get(register_page).post(register))
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+#[allow(dead_code)]
+struct HomePage {
+    logged_in: bool,
+    username: String,
 }
 
 #[derive(Template)]
@@ -43,6 +51,20 @@ pub async fn logout(jar: CookieJar) -> impl IntoResponse {
 
 async fn register_page() -> Result<Html<String>, AppError> {
     let html = RegisterPage.render()?;
+    Ok(Html(html))
+}
+
+async fn home(maybe_user: Option<User>) -> Result<Html<String>, AppError> {
+    let (logged_in, username) = match maybe_user {
+        Some(user) => (true, user.username().to_owned()),
+        None => (false, String::new()),
+    };
+
+    let html = HomePage {
+        logged_in,
+        username,
+    }
+    .render()?;
     Ok(Html(html))
 }
 
@@ -78,23 +100,21 @@ async fn register(
     let unauth_user = UnauthenticatedUser::new(request.username, request.password);
     let user = unauth_user.register(&repository).await?;
     let token = user.auth_token()?;
-    let cookie = Cookie::build(("token", token)).http_only(true);
+    let cookie = Cookie::build(("token", token)).path("/").http_only(true);
     Ok((jar.add(cookie), Redirect::to("/"))) // Replace with actual HTML rendering
-}
-
-async fn index(maybe_user: Option<User>) -> Result<Redirect, AppError> {
-    match maybe_user {
-        Some(_) => Ok(Redirect::to("/assets")),
-        None => Ok(Redirect::to("/login")),
-    }
 }
 
 #[derive(Template)]
 #[template(path = "assets.html")]
+#[allow(dead_code)]
 pub struct AssetsPage {
     owned_assets: Vec<OwnedAsset>,
     available_assets: Vec<Asset>,
     user: User,
+    username: String,
+    total_positions: usize,
+    total_current_value: f64,
+    total_delta: f64,
 }
 
 pub async fn assets(repository: Repository, user: User) -> Result<Html<String>, AppError> {
@@ -103,9 +123,20 @@ pub async fn assets(repository: Repository, user: User) -> Result<Html<String>, 
         repository.list_assets()
     )?;
 
+    let total_positions = owned_assets.len();
+    let total_current_value = owned_assets
+        .iter()
+        .map(|asset| asset.quantity_owned * asset.unit_value)
+        .sum();
+    let total_delta = owned_assets.iter().map(|asset| asset.value_delta).sum();
+
     let html = AssetsPage {
         owned_assets,
         available_assets,
+        username: user.username().to_owned(),
+        total_positions,
+        total_current_value,
+        total_delta,
         user,
     }
     .render()?;
@@ -119,7 +150,7 @@ struct PurchaseAssetForm {
     quantity: f64,
 }
 
-pub async fn purchase_asset(
+async fn purchase_asset(
     repository: Repository,
     user: User,
     Form(request): Form<PurchaseAssetForm>,
@@ -137,22 +168,9 @@ pub async fn purchase_asset(
 
 pub mod filters {
     use askama;
-    use time::{
-        OffsetDateTime, format_description::StaticFormatDescription, macros::format_description,
-    };
 
     #[askama::filter_fn]
-    pub fn human_datetime(
-        datetime: &OffsetDateTime,
-        _env: &dyn askama::Values,
-    ) -> askama::Result<String> {
-        const HUMAN_READABLE_FORMAT: StaticFormatDescription = format_description!(
-            "[year]-[month]-[day] [hour]:[minute]
-            "
-        );
-
-        datetime
-            .format(&HUMAN_READABLE_FORMAT)
-            .map_err(askama::Error::custom)
+    pub fn brl(value: &f64, _env: &dyn askama::Values) -> askama::Result<String> {
+        Ok(format!("R$ {:.2}", value))
     }
 }
